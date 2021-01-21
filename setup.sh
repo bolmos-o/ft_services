01952bfbb5212f4faea3fc42496d48340216b8ca
user42 <bolmos-o@student.42.fr>
2021-01-21 17:07:42 +0100

    01

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 minikube start --driver=docker
+# Deploy dashboard
+kubectl apply -f srcs/dashboard-adminuser.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
+# To get the token use :
+# kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
+# Install metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/config.yaml
